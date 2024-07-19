cd4d3f1584f4380a020ad88f944dbe6601f861fa
Philipp Neumann <p.neumann@supernice-dev.com>
2024-07-19 09:31:28 +0200

    update storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,5 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
-import path from "path";
+import path, { dirname, join } from "path";
 
 // const webpackDevConfFunc = require("../webpack.dev.js");
 const webpackDevConfFunc = require("../webpack.dev.js");
@@ -9,11 +9,11 @@ const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   staticDirs: ["../public"],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
-    "@storybook/addon-webpack5-compiler-swc",
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-onboarding"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-webpack5-compiler-swc"),
   ],
   webpackFinal: (config) => {
     // webpackFinal: async (config, { configType }) => {
@@ -39,12 +39,14 @@ const config: StorybookConfig = {
   },
 
   framework: {
-    name: "@storybook/react-webpack5",
+    name: getAbsolutePath("@storybook/react-webpack5"),
     options: { builder: {} },
   },
-  docs: {
-    autodocs: "tag",
-  },
+  docs: {},
 };
 
 export default config;
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}

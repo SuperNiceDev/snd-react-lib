57e2d33b0005b96b365a85ecd9eae0cdf58d9b71
Philipp Neumann <p.neumann@supernice-dev.com>
2024-10-15 19:26:32 +0200

    add mui component

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,12 @@
 import React, { ReactElement } from "react";
 
 import Text from "@src/components/atoms/Text";
+import MuiTest from "@src/components/molecules/MuiTest";
 import ContextProviderTasks from "@src/context/ContextProviderTasks";
 import "@src/css/base.css";
 import "@src/css/base.scss";
 import "@src/css/reset.css";
+import "@src/css/tailwind.css";
 import AddTask from "@src/stories/context/ContextProviderTasks/AddTask";
 import TaskList from "@src/stories/context/ContextProviderTasks/TaskList";
 
@@ -12,13 +14,16 @@ const App = (): ReactElement => {
   return (
     <div className="App">
       <div className="ctn">
-        <Text text="my text" fff={0} />
+        <Text text="my text" />
       </div>
       <ContextProviderTasks>
         <h1>Day off in Kyoto</h1>
         <AddTask />
         <TaskList />
       </ContextProviderTasks>
+      <div className="ctn">
+        <MuiTest />
+      </div>
     </div>
   );
 };

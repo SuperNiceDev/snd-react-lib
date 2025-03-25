ed966d3c1f6561b633b516001c3a171a93d322fc
Philipp Neumann <p.neumann@supernice-dev.com>
2025-03-25 10:00:02 +0100

    major version upgrade except tailwind

--- src/stories/context/ContextTasks/TaskList.tsx
+++ src/stories/context/ContextTasks/TaskList.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 
-import { useBaseContext, useContextBaseDispatch } from "../../../context/ContextBase";
+import {
+  useBaseContext,
+  useContextBaseDispatch,
+} from "../../../context/ContextBase";
 
 export default function TaskList() {
   const tasks = useBaseContext();

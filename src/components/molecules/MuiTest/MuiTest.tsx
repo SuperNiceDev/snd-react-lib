862ee28d32c45020e68d9b313331ffb95958df9b
Philipp Neumann <p.neumann@supernice-dev.com>
2025-04-19 11:06:02 +0200

    update layout

--- src/components/molecules/MuiTest/MuiTest.tsx
+++ src/components/molecules/MuiTest/MuiTest.tsx
@@ -11,43 +11,37 @@ import css from "./MuiTest.module.scss";
 export default function MuiTest() {
   return (
     <div className={`${css.root}`}>
-      {`<MuiTest>`}
+      <code>{`<MuiTest> (snd-react-lib)`}</code>
 
       <main className={`${css.main} tw:px-4`}>
         <div className="tw:mb-2">
-          {`<Text>`}
-          <Text className="tw:text-lime-700_" text="Text text prop test" />
-          {`</Text>`}
+          <code>{`<Text> (snd-react-lib)`}</code>
+          <Text className="tw:text-lime-700_" text="Text prop text test" />
+          <code>{`</Text>`}</code>
         </div>
 
-        <div className="line">{`// -------------------------`}</div>
-
         <div className="tw:mb-2">
-          {`<InputText>`}
+          <code>{`<InputText> (snd-react-lib)`}</code>
           <div>
             <InputText
-              className="tw:px-2 tw:text-lime-700_ tw:bg-gray-700_ tw:border tw:border-lime-700"
-              placeholder="InputText placeholder prop test"
-              // value="InputText value prop test"
+              className="tw:px-2 tw:text-lime-700_ tw:bg-gray-700_ tw:border tw:border-lime-700_"
+              placeholder="InputText prop placeholder test"
+              // value="InputText prop value test"
             />
           </div>
-          {`</InputText>`}
+          <code>{`</InputText>`}</code>
         </div>
 
-        <div className="line">{`// -------------------------`}</div>
-
         <div className="tw:mb-2">
-          {`<h1>`}
+          <code>{`<h1>`}</code>
           <h1 className="tw:text-3xl tw:font-bold tw:text-lime-700_">
             {`Tailwind <h1 />`}
           </h1>
-          {`</h1>`}
+          <code>{`</h1>`}</code>
         </div>
 
-        <div className="line">{`// -------------------------`}</div>
-
         <div className="tw:mb-2">
-          {`<Typography variant="h1">`}
+          <code>{`<Typography variant="h1">`}</code>
           <Typography
             className="tw:text-3xl tw:font-bold tw:text-lime-700_"
             variant="h1"
@@ -55,13 +49,11 @@ export default function MuiTest() {
           >
             {`Mui <Typography variant="h1" />`}
           </Typography>
-          {`</Typography>`}
+          <code>{`</Typography>`}</code>
         </div>
 
-        <div className="line">{`// -------------------------`}</div>
-
         <div className="tw:mb-2">
-          {`<Stack>`}
+          <code>{`<Stack>`}</code>
           <Stack
             className="tw:my-4 tw:text-3xl tw:font-bold tw:underline tw:text-lime-700_"
             spacing={2}
@@ -73,11 +65,11 @@ export default function MuiTest() {
               Button variant Text
             </Button>
           </Stack>
-          {`</Stack>`}
+          <code>{`</Stack>`}</code>
         </div>
       </main>
 
-      {`</MuiTest>`}
+      <code>{`</MuiTest>`}</code>
     </div>
   );
 }
